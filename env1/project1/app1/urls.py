f390f09fcab1261da8196f1cd4d4d4cfdf88df73
radhamouni <127676379+radhamouni@users.noreply.github.com>
2023-04-07 09:53:52 -0700

    Update urls.py

--- env1/project1/app1/urls.py
+++ env1/project1/app1/urls.py
@@ -3,9 +3,14 @@ from . import views
 
 urlpatterns = [
     path('',views.default),
+    path('guide',views.guide),
     path('home',views.default),
+    path('findit',views.default),
     path('subjects',views.subjects),
-    path('view1', views.view1),
-    path('view3', views.view3),
-    path('view2', views.view2),
+    path('guide', views.guide),
+    # path('view3', views.view3),
+    # path('view2', views.view2),
+    path('findit/<int:id_no>',views.findit),
+    path('exam',views.exam),
+    path('cal',views.cal),
 ]
